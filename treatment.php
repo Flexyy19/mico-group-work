d87de7874b7dc938a2e7e33ed84b3515fc18c504
Irakli Kereselidze <rmgwork9@gmail.com>
2025-03-29 17:59:22 +0400

    Add-treatment

---
+++ treatment.php
@@ -0,0 +1,9 @@
+<?php include_once "./header.php" ?>
+<!-- end header section -->
+
+<!-- treatment section -->
+<?php include "./indexhtmls.php/treatmenthtml.php" ?>
+<!-- end treatment section -->
+
+<!-- footer section -->
+<?php include_once "./footer.php" ?>
